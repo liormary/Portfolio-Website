367d2679eb10698b8fc566ace347fabe74612735
liormary <liorms3@gmail.com>
2024-10-07 22:51:23 +0300

    text changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.png'
-import { FaAward } from "react-icons/fa6";
+import { GrTechnology } from "react-icons/gr";
 import { FiUsers } from "react-icons/fi";
-import { VscFolderLibrary } from "react-icons/vsc";
+import { HiOutlinePaintBrush } from "react-icons/hi2";
 
 const About = () => {
   return (
@@ -21,26 +21,26 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon' />
-              <h5>Experience</h5>
-              <small>3+ Years</small>
+              <GrTechnology className='about__icon' />
+              <h5>Tech Enthusiast              </h5>
+              <small>Passionate about impactful web applications</small>
             </article>
 
             <article className='about__card'>
-              <FiUsers  className='about__icon' />
-              <h5>Clients</h5>
-              <small>200+ Clients Worldwide</small>
+              <HiOutlinePaintBrush  className='about__icon' />
+              <h5>Creative Solver</h5>
+              <small>Blending creativity with technical skills</small>
             </article>
 
             <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
-              <h5>Projects</h5>
-              <small>80+ Completed</small>
+              <FiUsers className='about__icon' />
+              <h5>Team Player</h5>
+              <small>Thriving in collaborative environments</small>
             </article>
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec erat id neque ultricies ultricies. In non velit in massa pharetra bibendum. Sed at risus at neque lobortis convallis. Integer vel semper nunc. In hac habitasse platea dictumst. Donec at varius nunc, non viverra turpis. Integer vel neque eget dolor consequat consectetur. Donec facilisis, lectus non viverra consectetur, mauris neque consectetur neque, in scelerisque nisi velit id neque. Nulla facilisi.
+            I am a passionate Full Stack/Front-End Developer dedicated to creating intuitive web applications that enhance user experiences. With a solid foundation in web technologies and a collaborative mindset, I thrive in dynamic environments where I can tackle challenges and deliver innovative solutions. I’m currently seeking my next opportunity to contribute my skills and creativity to impactful projects while continuing to grow in the tech industry.
           </p>
 
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
