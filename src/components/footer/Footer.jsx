7c1a43ee96df8cdb40650daefbae22a694bc675e
liormary <liorms3@gmail.com>
2024-10-09 21:41:36 +0300

    footer component jsx and css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className='footer'>
+      <a href="" className='footer__logo'>AAAA</a>
+
+      <ul className='permalinks'>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#recommendations">Recommendations</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href='https://www.linkedin.com/in/lior-mary/' target='_blank'><BsLinkedin /></a>
+      <a href='https://github.com/liormary' target='_blank'><BsGithub /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2024 Lior Mary. All Rights Reserved.</small>
+      </div>
     </footer>
   )
 }
