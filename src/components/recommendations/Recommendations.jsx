f18dce44012703912797e703335978486d66f290
liormary <liorms3@gmail.com>
2024-10-09 15:44:02 +0300

    recommendations component jsx and css

--- src/components/recommendations/Recommendations.jsx
+++ src/components/recommendations/Recommendations.jsx
@@ -30,7 +30,11 @@ const Recommendations = () => {
             <img src={TOMER} alt='profile photo' />
           </div>
           <h5 className='client__name'>Tomer Grossman</h5>
-          <small className='client__review'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quidem mollitia, fuga sed accusantium vel ipsa similique ratione eaque, aliquid, sequi qui dolores et expedita enim quas quaerat dolor recusandae.</small>
+          <small className='client__review'>As Lior's former manager I had the privilege of working with Lior on several projects, and her impact on our team was remarkable. Lior is an outstanding team player who consistently fosters collaboration and open communication. She has a natural ability to work seamlessly with others, always contributing her insights and supporting her teammates. She brought fresh ideas, solved complex problems, and helped drive every project she was part of toward success. 
+
+One of Lior's standout qualities is her incredible sense of responsibility. She takes initiative, meets deadlines with precision, her dedication to delivering high-quality work is unwavering, and she consistently exceeds expectations.
+
+I have no doubt that she will continue to excel in any future endeavors, and I highly recommend her to any team or organization.</small>
         </SwiperSlide>
 
         <SwiperSlide className='recommendation'>
