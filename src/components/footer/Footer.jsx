88acfc57315397fb879a61fd3aa2a15bc88413bc
liormary <liorms3@gmail.com>
2024-10-11 02:38:53 +0300

    edits for deployment

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,7 +13,6 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#education">Education</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
